54ebfe65ed2953b5f1dfb017ca484e79d8388825
Artem Trukhanov <trukhanovartem@me.com>
2026-02-11 19:19:59 +0400

    Fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,13 +6,22 @@ import * as path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    port: 5173,
+    allowedHosts: ['localhost:5173', 'nas.local', 'localhost:3001'],
+    proxy: {
+      '/api': 'http://games-browser-backend:3001',
+    },
+  },
   server: {
+    allowedHosts: ['nas.local', 'localhost:3001'],
     proxy: {
       '/api': 'http://games-browser-backend:3001',
     },
